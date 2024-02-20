24e4440c9bbb703cd6427f855686560e66dec390
vivekop0 <sarkarjii534@gamil.com>
2024-02-20 18:19:17 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+const  JWT_SECRET ="secret"
+module.exports={
+    JWT_SECRET:JWT_SECRET
+}
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
-const express = require("express");
 
+// backend/index.js
+const express = require('express');
+const cors = require("cors");
+const rootRouter = require("./routes/index");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1", rootRouter);
+
+app.listen(5000,()=>{
+    console.log("port started in 5000");
+});
